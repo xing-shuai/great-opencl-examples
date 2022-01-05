ca3d15578b03ff94a44d765e5d997b5146616fa8
shuai.xing <xs_xingshuai@126.com>
2022-01-05 09:09:47 +0800

    1.add CMakeLists.txt 2.modify CMakeLists.txt under hello_world folder and run the code

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -1,20 +1,21 @@
 #include <CL/cl.hpp>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
-cl::Device get_default_device(){
-    
+cl::Device get_default_device() {
+
     /**
      * Search for all the OpenCL platforms available and check
      * if there are any.
      * */
 
-    std::vector<cl::Platform> platforms;
-    cl::Platform::get(&platforms);
+    std::vector< cl::Platform > platforms;
+    cl::Platform::get( &platforms );
 
-    if (platforms.empty()){
+    if ( platforms.empty() ) {
         std::cerr << "No platforms found!" << std::endl;
-        exit(1);
+        exit( 1 );
     }
 
     /**
@@ -22,13 +23,13 @@ cl::Device get_default_device(){
      * there are any available.
      * */
 
-    auto platform = platforms.front();
-    std::vector<cl::Device> devices;
-    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+    auto                      platform = platforms.front();
+    std::vector< cl::Device > devices;
+    platform.getDevices( CL_DEVICE_TYPE_ALL, &devices );
 
-    if (devices.empty()){
+    if ( devices.empty() ) {
         std::cerr << "No devices found!" << std::endl;
-        exit(1);
+        exit( 1 );
     }
 
     /**
@@ -38,7 +39,7 @@ cl::Device get_default_device(){
     return devices.front();
 }
 
-int main(){
+int main() {
 
     /**
      * Select a device.
@@ -50,45 +51,44 @@ int main(){
      * Read OpenCL kernel file as a string.
      * */
 
-    std::ifstream hello_world_file("hello_world.cl");
-    std::string src(std::istreambuf_iterator<char>(hello_world_file), (std::istreambuf_iterator<char>()));
+    std::ifstream hello_world_file( "../../hello_world/hello_world.cl" );
+    std::string   src( std::istreambuf_iterator< char >( hello_world_file ), ( std::istreambuf_iterator< char >() ) );
 
     /**
      * Compile the program which will run on the device.
      * */
 
-    cl::Program::Sources sources(1, std::make_pair(src.c_str(), src.length() + 1));
-    cl::Context context(device);
-    cl::Program program(context, sources);
+    cl::Program::Sources sources( 1, std::make_pair( src.c_str(), src.length() + 1 ) );
+    cl::Context          context( device );
+    cl::Program          program( context, sources );
 
     auto err = program.build();
-    if(err != CL_BUILD_SUCCESS){
-        std::cerr << "Build Status: " << program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(device) 
-        << "Build Log:\t " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << std::endl;
-        exit(1);
+    if ( err != CL_BUILD_SUCCESS ) {
+        std::cerr << "Build Status: " << program.getBuildInfo< CL_PROGRAM_BUILD_STATUS >( device ) << "Build Log:\t " << program.getBuildInfo< CL_PROGRAM_BUILD_LOG >( device ) << std::endl;
+        exit( 1 );
     }
-    
+
     /**
      * Create buffers and allocate memory on the device.
      * */
 
-    char buf[16];
-    cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
-    cl::Kernel kernel(program, "helloWorld", nullptr);
+    char       buf[ 16 ];
+    cl::Buffer memBuf( context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof( buf ) );
+    cl::Kernel kernel( program, "helloWorld", nullptr );
 
     /**
      * Set kernel argument.
      * */
 
-    kernel.setArg(0, memBuf);
+    kernel.setArg( 0, memBuf );
 
     /**
      * Run the kernel function and collect its result.
      * */
 
-    cl::CommandQueue queue(context, device);
-    queue.enqueueTask(kernel);
-    queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
+    cl::CommandQueue queue( context, device );
+    queue.enqueueTask( kernel );
+    queue.enqueueReadBuffer( memBuf, CL_TRUE, 0, sizeof( buf ), buf );
 
     /**
      * Print result.
